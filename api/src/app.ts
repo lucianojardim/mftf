301812ee98ef811735ed4a201ef4e77ff5f02824
Luciano Jardim <lucianocjardim@gmail.com>
2017-09-24 07:07:58 -0500

    V.0.3 API capable of basic functions

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,23 +1,84 @@
-import { CorsMiddleware } from './middleware/cors-middleware';
 import * as express from 'express';
 
 import 'reflect-metadata';
 import { Action, createExpressServer, MiddlewareMetadata, useContainer, useExpressServer } from 'routing-controllers';
 import {Container} from "typedi";
 
+let jwt = require("express-jwt");
+let rsaValidation = require("auth0-api-jwt-rsa-validation");
+
+let morgan = require("morgan");
+// let fs = require("fs");
+// let path = require("path");
+// let rfs = require("rotating-file-stream");
+
+let cors = require("cors");
+
+let helmet = require("helmet");
+
+let compression = require("compression");
+
 /**
  * Setup routing-controllers to use typedi container.
  */
 useContainer(Container);
 
+const app = express();
+
+/**
+ * Define logging
+ */
+// const logDirectory = path.join(__dirname, 'log')
+// // ensure log directory exists 
+// fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// // create a rotating write stream 
+// const accessLogStream = rfs('access.log', {
+//     interval: '1d', // rotate daily 
+//     maxFiles: 3,
+//     path: logDirectory
+//   });
+// app.use(morgan("combined", {stream: accessLogStream}));
+app.use(morgan("combined"));
+
+app.use(helmet());
+
+const jwtCheck = jwt({
+    secret: rsaValidation(),
+    algorithms: ['RS256'],
+    issuer: "https://mftfapi.auth0.com/",
+    audience: "mftfapi"
+  });
+app.use(jwtCheck);
+// // If we do not get the correct credentials, we’ll return an appropriate message
+// app.use(function (err: express.Errback, req: express.Request, res: express.Response, next: express.NextFunction) {
+//     if (err.name === 'UnauthorizedError') {
+//       res.status(401).json({message:'Missing or invalid token'});
+//     }
+//   });
+
+app.use(compression());
+
+// const whitelist = ['localhost', 'mftf-ui'];
+// const corsOptions = {
+//   origin: function (origin: any, callback:any) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }
+const corsOptions = "*";
+app.use(cors(corsOptions));
+
 /**
  * We create a new express server instance.
  * We could have also use useExpressServer here to attach controllers to an existing express instance.
  */
-const expressApp = createExpressServer({
+const expressApp = useExpressServer(app,{
     authorizationChecker: async (action: Action, roles: string[]) => {
         let authorize: boolean = false;
-        const permissions = ["test"];
+        const permissions = ["write:students"];
         for (var i = 0; i < permissions.length; i++) {
             if (action.request.user.scope.includes(permissions[i])) {
                 authorize = true;
@@ -25,7 +86,7 @@ const expressApp = createExpressServer({
                 authorize = false;
             }
         }
-        return authorize;
+        return Promise.resolve(authorize);
     },
     // currentUserChecker: async (action: Action) => {
     //     // here you can use request/response objects from action
@@ -34,13 +95,8 @@ const expressApp = createExpressServer({
     //     const token = action.request.headers["authorization"];
     //     return getEntityManager().findOneByToken(User, token);
     // },
-    cors: true,
-
     controllers: [
         __dirname + "/controller/**/*.js"
-    ],
-    middlewares: [
-        __dirname + "/middleware/**/*.js"
     ]
 });
 
